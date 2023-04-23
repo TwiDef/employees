0f79cea1bf2bf157592b68df4e20dada740a1381
Def <deferenseo@gmail.com>
2023-04-23 19:39:38 +0300

    added posibility delete employee from list

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,15 @@
 import EployeesListItem from "../employees-list-item/employees-list-item"
 import './employees-list.css'
 
-const EployeesList = ({ data }) => {
+const EployeesList = ({ data, onDelete }) => {
 
     const elements = data.map(item => {
         const { id, ...itemProps } = item
         return (
-            <EployeesListItem key={id} {...itemProps} />
+            <EployeesListItem
+                key={id}
+                {...itemProps}
+                onDelete={() => onDelete(id)} />
         )
     })
 
