2e6b99949202f12dc24fe05c02dec23e6a47f586
Def <deferenseo@gmail.com>
2023-04-26 15:36:25 +0300

    added filter mechanics for employee list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ class App extends Component {
                 { name: "Ann Rise", salary: 3000, increase: false, rise: false, id: 2 },
                 { name: "Oli Sykes", salary: 700, increase: true, rise: false, id: 3 },
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4
     }
@@ -73,11 +74,26 @@ class App extends Component {
         this.setState({ term })
     }
 
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise)
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000)
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({ filter })
+    }
+
     render() {
-        const { data, term } = this.state
+        const { data, term, filter } = this.state
         const employees = this.state.data.length
         const increased = this.state.data.filter(item => item.increase).length
-        const visibleData = this.searchEmp(data, term)
+        const visibleData = this.filterPost(this.searchEmp(data, term), filter)
 
         return (
             <div className="app">
@@ -87,7 +103,9 @@ class App extends Component {
 
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-                    <AppFilter />
+                    <AppFilter
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect} />
                 </div>
 
                 <EployeesList
